d556ec38f47c429f08d807dafd641ad6cf1f4109
ljk <731236067@qq.com>
2019-06-15 20:58:19 +0800

    添加ssm拆分到Git上

---
+++ day01_maven02_parent/day01_maven02_dao/src/main/java/com/itheima/dao/ItemsDao.java
@@ -0,0 +1,11 @@
+package com.itheima.dao;
+
+import com.itheima.domain.Items;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemsDao {
+    @Select("select * from items where id = #{id}")
+    public Items findById(Integer id);
+}
